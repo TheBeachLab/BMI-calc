abce7c58c5c02e430ab9f20cbba218eff197f904
Fran <hola@beachlab.org>
2019-12-30 02:53:53 +0100

    refactor widgets

--- lib/input.dart
+++ lib/input.dart
@@ -19,61 +19,39 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Color(0xff1D1F33),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Color(0xff1D1F33),
-                      ),
-                    ),
-                  ),
+                  MyCard(tone: Color(0xff1D1F33)),
+                  MyCard(tone: Color(0xff1D1F33)),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Color(0xff1D1F33),
-                ),
-              ),
-            ),
+            MyCard(tone: Color(0xff1D1F33)),
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Color(0xff1D1F33),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Color(0xff1D1F33),
-                      ),
-                    ),
-                  ),
+                  MyCard(tone: Color(0xff1D1F33)),
+                  MyCard(tone: Color(0xff1D1F33)),
                 ],
               ),
             ),
           ],
         ));
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final Color tone; // property of my card
+  MyCard({@required this.tone}); // constructor with required property
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: tone,
+        ),
+      ),
+    );
   }
 }
